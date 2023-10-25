6dc891ab6dc088aae84aaab8838fdef60e38df31
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-10-25 18:07:15 -0400

    added functions

--- server.js
+++ server.js
@@ -26,9 +26,58 @@ const selection = function () {
             "Add an employee",
             "Update an employee role"
         ]
-
         }
-    ]
+     ])
+     .then(function(answers) {
+        switch (answers.prompt) {
 
-    )
+            case "View all departments";
+            viewAllDepts();
+            break;
+
+            case "View all roles";
+            viewRoles();
+            break;
+
+            case "View all employees";
+            viewEmployees();
+            break;
+
+            case "Add a department";
+            addDept();
+            break;
+
+            case "Add a role";
+            addRole();
+            break;
+
+            case "Add an employee";
+            addEmployee();
+            break;
+
+            case "Update an employee role";
+            updateEmployee();
+            break;
+        }
+})
+};
+selection();
+
+function viewAllDepts () {
+    connection.query(`SELECT * FROM department`, function (err, result) {
+        if (err) throw err;
+        console.table(result);
+        selection();
+    })
+}
+
+function viewRoles () {
+    connection.query(`SELECT * FROM role`, function (err, result) {
+        if (err) throw err;
+        console.table(result);
+        selection();
+    })
+}
+
+
 }
