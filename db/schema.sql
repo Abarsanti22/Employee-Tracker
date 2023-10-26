b76b8c42745f850ca467b1985f18a37a377d10b1
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-10-26 15:45:29 -0400

    updated employee table schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,27 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE department
+CREATE TABLE department (
  id INT NOT NULL AUTO_INCREMENT,
  title VARCHAR(30) NOT NULL,
  PRIMARY KEY
+);
 
-CREATE TABLE role
+
+CREATE TABLE role (
  id INT NOT NULL AUTO_INCREMENT,
  title VARCHAR(30) NOT NULL,
  salary DECIMAL NOT NULL,
  department_id INT,
  PRIMARY KEY
- 
+);
 
-CREATE TABLE employee
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+);
+
+);
